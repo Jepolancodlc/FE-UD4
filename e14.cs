5a63903208e1814ee3a0e2d10ba089c35cd86129
Jepolancodlc@gmail.com <Jepolancodlc@gmail.com>
2020-12-28 13:29:39 +0100

    e14

---
+++ e14.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UD4_e14
+{
+    class e14
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el radio");
+            double radio = Convert.ToDouble(Console.ReadLine());
+            double total = Math.Pow(radio, 2) * Math.PI;
+
+
+            Console.WriteLine("El area del circulo es de {0}", total);
+        }
+    }
+}
