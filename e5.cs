407b602538284ba0d1ef09408c1a035cb0e4e2ae
Jepolancodlc@gmail.com <Jepolancodlc@gmail.com>
2020-12-28 13:34:33 +0100

    e5

---
+++ e5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UD4_e5
+{
+    class e5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba un dia de la semana:");
+            string dia =  Console.ReadLine();
+
+            switch (dia.ToLower())
+            {
+                case "lunes":
+                case "martes":
+                case "miercoles":
+                case "jueves":
+                case "viernes":
+                    Console.WriteLine("{0} no es fin de semana", dia);
+                    break;
+                case "sabado":
+                case "domingo":
+                    Console.WriteLine("{0} es fin de semana", dia);
+                    break;
+                default:
+                    Console.WriteLine("{0} no es un dia valido", dia);
+                    break;
+            }
+        }
+    }
+}
