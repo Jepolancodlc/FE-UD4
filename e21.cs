90a15c63e1e0cd3604b6a2923b3636f317fa8225
Jepolancodlc@gmail.com <Jepolancodlc@gmail.com>
2020-12-28 13:32:29 +0100

    e21

---
+++ e21.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UD4_e21
+{
+    class e21
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba un dia de la semana:");
+            string dia = Console.ReadLine();
+
+            switch (dia.ToLower())
+            {
+                case "lunes":
+                case "martes":
+                case "miercoles":
+                case "jueves":
+                case "viernes":
+                case "sabado":
+                    Console.WriteLine("{0} es laborable", dia);
+                    break;
+                case "domingo":
+                    Console.WriteLine("{0} no es laborable", dia);
+                    break;
+                default:
+                    Console.WriteLine("{0} no es un dia valido", dia);
+                    break;
+            }
+        }
+    }
+}
