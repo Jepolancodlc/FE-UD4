6ebb7ecf9f297d8b89b473e491e0d42cefbfd14b
Jepolancodlc@gmail.com <Jepolancodlc@gmail.com>
2020-12-28 13:31:09 +0100

    e18

---
+++ e18.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UD4_e18
+{
+    class e18
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
