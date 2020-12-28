ee616c836c0937a0c900cfb184ac4c5b25993ef4
Jepolancodlc@gmail.com <Jepolancodlc@gmail.com>
2020-12-28 13:31:49 +0100

    e2

---
+++ e2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UD4_e2
+{
+    class e2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe un nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Escribe el nombre de una ciudad");
+            string ciudad = Console.ReadLine();
+            Console.WriteLine("Hola {0} bienvenido a {1}", nombre, ciudad);
+          
+        }
+    }
+}
