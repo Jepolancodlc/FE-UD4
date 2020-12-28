01f5931bb2342722276e82a9d38e2fd43af0e399
Jepolancodlc@gmail.com <Jepolancodlc@gmail.com>
2020-12-28 13:33:27 +0100

    e3

---
+++ e3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UD4_e3
+{
+    class e3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("¿Cuantos años tienes?");
+            string años = Console.ReadLine();
+            Console.WriteLine("Te llamas {0} y tienes {1}", nombre, años);
+        }
+    }
+}
