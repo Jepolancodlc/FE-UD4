f5eb5cec7078b8293a89dfdec6b4057deaff33fc
Jepolancodlc@gmail.com <Jepolancodlc@gmail.com>
2020-12-28 13:30:51 +0100

    e17

---
+++ e17.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UD4_e17
+{
+    class e17
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i < 101)
+            {
+
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
